3f9c5f7707833afbbb892bfed5d9507e6590f3d2
unknown <james.bockman@live.com.au>
2019-02-28 08:44:49 +1030

    Messing around with the training, have predictions processing properly, need to build a pipeline for groung truth images to be converted to labeled pixels and a function to visualise masks.

--- Scripts/pytorchHelpers/visual.py
+++ Scripts/pytorchHelpers/visual.py
@@ -1,6 +1,7 @@
 # Functions to assist with visualising pytorch tensors
 
 import numpy as np
+import matplotlib.pyplot as plt
 from torchvision import utils
 
 # Converts a pytorch tensor stack to a RGB colour image numpy array
@@ -11,3 +12,27 @@ def tensorToNumpy(imageTensor):
     # avoid the wrath of matplotlib
     grid = np.clip(grid, 0, 1)
     return grid
+
+# Displays an image from an inputted tensor
+def plotOutputTensor(tensor):
+    plt.figure(figsize=(16, 14))
+    plt.axis('off')
+    for number1, pred in enumerate(tensor):
+        pred = pred.detach().to("cpu")
+        for number2, channel in enumerate(pred):
+            channel = tensorToNumpy(channel)
+            plt.imshow(channel)
+            plt.savefig('Figures\\Prediction-'+str(number1)+
+                        '-channel-'+str(number2)+'.png')
+    return
+
+# Displays an image from an inputted tensor
+def plotPredictionTensor(tensor):
+    plt.figure(figsize=(16, 14))
+    plt.axis('off')
+    for number1, pred in enumerate(tensor):
+        pred = pred.detach().to("cpu")
+        pred = tensorToNumpy(pred)
+        plt.imshow(pred)
+        plt.savefig('Figures\\PredictionOverall-' + str(number1) + '.png')
+    return
