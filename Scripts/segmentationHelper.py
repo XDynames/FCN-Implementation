15f20bf5eb62dee41854b1815c2ffac7ad269295
unknown <james.bockman@live.com.au>
2019-04-01 06:36:01 +1030

    Started implementing Segmentor, a class to help translate between images and label array

---
+++ Scripts/segmentationHelper.py
@@ -0,0 +1,39 @@
+# Like a dementor, but useful for segmentation tasks
+
+# Built for translation of ground truth images into label arrays and vice verse in pytorch
+
+import numpy as np
+
+class Segmentor:
+    
+    # Initialise the segmentor with a corresponding list of colours
+    # labels and names ie. colours[0] -> lables[0] -> names[0]
+    def __init__(self, colours, labels, names):
+        self._colours = [[0, 0, 0]] + colours
+        self._labels = [0] + labels
+        self._names = ['Background'] + names
+    
+
+    # Maps the list of colours in the image to their respective
+    # labels and retuns the resulting label array
+    def mapColoursToLabels(self, image): 
+        labeled = np.zeros((image.shape[0], image.shape[1]), dtype = np.uint8)
+        # Check if the pixel matches any of the labeled colours      
+        for colour, label in zip(self._colours, self._labels):
+            mask = np.all(image == colour, axis = -1)
+            labeled = np.where(mask == True, label, labeled)
+        return labeled
+
+    # Wrapper for mapColoursToLabels so it can be directly used as a pytorch
+    # transform, taking in a PIL image and return a numpy array of labels.
+    # torchvision.transforms.Lambda(lambda x : Segmentor.mapColoursToLabelsPIL(x))
+    def mapColoursToLabelsPIL(self, imagePIL): 
+        image = np.array(imagePIL)
+        labeled = self.mapColoursToLabels(image)
+        return labeled
+    
+    
+    # From an numpy array of labels creates an RGB numpy array image
+    def mapLabelsToColours(self, labelArray):
+        # Constrcuts a colour image using the labels in the array
+        return np.array(self._colours)[labelArray]
